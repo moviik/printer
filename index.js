ceec9e425cf18a40f5c34c8bb1b849610c435da1
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-10-09 18:44:07 +0100

    dev: example with mik-dispenser 3990c2c5-1259-481e-b2bd-2c7c2b340335

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 require('app-module-path/cwd')
-const ipc = require('node-ipc')
-
-ipc.config.id = 'mik_printer'
-ipc.config.silent = true
-ipc.config.maxConnections = 1
-ipc.serve(() => {
-  const server = ipc.server
-  server.on('connect', (socket) => {
-
-  })
-  server.on('client.id', (object, data) => {
-    console.log('received from client')
-    console.log(object)
-  })
-})
-
-ipc.server.start()
 
 global.lo_ = require('lodash')
 
+const ipc = require('node-ipc')
 const StatusManager = require('lib/cesmlm/status_manager')
 const PrinterError = require('lib/errors/printer_error')
 const Modus3Adapter = require('lib/adapter/modus3_adapter')
 
-const statusManager = new StatusManager(Modus3Adapter)
-const statusErrors = Modus3Adapter.getStatusErrors()
-const statusParser = Modus3Adapter.getStatusParser()
+function configIpcServer () {
+  return new Promise((resolve, reject) => {
+    ipc.config.id = 'mik_printer'
+    ipc.config.silent = true
+    ipc.config.maxConnections = 1
+    ipc.serve(() => {
+      const server = ipc.server
+      server.on('connect', (socket) => {
 
-function openPrinter () {
-  try {
-    statusManager.openPrinter()
-  } catch (error) {
-    if (!(error instanceof PrinterError)) {
-      throw error
-    }
-    console.log(`Printer not available: ${statusErrors.toString(error.code)}`)
-  }
+      })
+      server.on('client.id', (socket, payload) => {
+        resolve(server)
+      })
+    })
+    ipc.server.start()
+  })
 }
 
-openPrinter()
+configIpcServer().then((server) => {
+  const statusManager = new StatusManager(Modus3Adapter)
+  const statusErrors = Modus3Adapter.getStatusErrors()
+  const statusParser = Modus3Adapter.getStatusParser()
 
-statusManager.on('printerDisconnected', (error) => {
-  console.log('printer Disconnected')
-  console.log(statusErrors.toString(error.code))
-})
+  function openPrinter () {
+    try {
+      statusManager.openPrinter()
+    } catch (error) {
+      if (!(error instanceof PrinterError)) {
+        throw error
+      }
+      console.log(`Printer not available: ${statusErrors.toString(error.code)}`)
+    }
+  }
 
-statusManager.on('statusChanged', async (rawStatus) => {
-  const parsedStatus = statusParser.parse(rawStatus)
-  console.log(statusParser.toStringArray(parsedStatus))
+  openPrinter()
+
+  statusManager.on('statusChanged', async (rawStatus) => {
+    const status = statusParser.toStringArray(statusParser.parse(rawStatus))
+    server.broadcast('printer.status', { status })
+  })
+
+  statusManager.on('printerDisconnected', (error) => {
+    const error_code = statusErrors.toString(error.code)
+    server.broadcast('printer.disconnected', { error_code })
+  })
 })
