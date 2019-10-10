7485695643b98a63a8711c9b3f64e25380e14cbc
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-10-10 10:19:41 +0100

    dev: example with mik-dispenser 96827c85-7c90-4646-b478-e33d778e2c89

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('app-module-path/cwd')
 global.lo_ = require('lodash')
 
 const ipc = require('node-ipc')
+
 const StatusManager = require('lib/cesmlm/status_manager')
+const XmlManager = require('lib/cpt/xml_manager')
 const PrinterError = require('lib/errors/printer_error')
 const Modus3Adapter = require('lib/adapter/modus3_adapter')
 
@@ -17,8 +19,17 @@ function configIpcServer () {
       server.on('connect', (socket) => {
 
       })
-      server.on('client.id', (socket, payload) => {
+      server.on('client.id', (payload, socket) => {
         resolve(server)
+      })
+      server.on('printer.print', (payload, socket) => {
+        try {
+          server.xmlManager.setXmlTagValue('TextBox0.text', payload.label)
+          server.xmlManager.printXml()
+          server.broadcast('printer.print_reply', { success: true })
+        } catch (error) {
+          server.broadcast('printer.print_reply', { success: false, error_code: Modus3Adapter.getPrintErrors().toString(error.code) })
+        }
       })
     })
     ipc.server.start()
@@ -30,14 +41,16 @@ configIpcServer().then((server) => {
   const statusErrors = Modus3Adapter.getStatusErrors()
   const statusParser = Modus3Adapter.getStatusParser()
 
+  const xmlManager = new XmlManager(Modus3Adapter)
+  server.xmlManager = xmlManager
+
   function openPrinter () {
     try {
       statusManager.openPrinter()
+      xmlManager.openPrinter()
+      xmlManager.setXmlFile('lib/ticket_template/Moviik.xml')
     } catch (error) {
-      if (!(error instanceof PrinterError)) {
-        throw error
-      }
-      console.log(`Printer not available: ${statusErrors.toString(error.code)}`)
+      server.broadcast('printer.open_error: ', error.code)
     }
   }
 
