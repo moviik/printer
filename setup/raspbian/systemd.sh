5c72928e0be29096aaa768c9e00251688f1f97ec
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-10-14 19:08:17 +0100

    deps: Systemd services install script

---
+++ setup/raspbian/systemd.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if [ "$(id -u)" != 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+cp ./ mik_printer.service /etc/systemd/system
+cp ./ mik_dispenser.service /etc/systemd/system
+
+systemctl enable mik_printer
+systemctl enable mik_dispenser
