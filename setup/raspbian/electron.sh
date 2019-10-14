7f29ceef1ea46a48f871a5e21b15a849fec2c927
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-10-14 19:08:04 +0100

    refactor: node install folder

--- setup/raspbian/electron.sh
+++ setup/raspbian/electron.sh
@@ -1,3 +1,5 @@
-wget https://nodejs.org/dist/v10.11.0/node-v10.11.0-linux-armv7l.tar.xz
-tar -xvf node-v10.11.0-linux-armv7l.tar.xz
-export PATH=${PATH}:$(pwd)/node-v10.11.0-linux-armv7l/bin
+#!/bin/sh
+
+wget https://nodejs.org/dist/v10.11.0/node-v10.11.0-linux-armv7l.tar.xz -P /home/pi
+tar -xvf /home/pi/node-v10.11.0-linux-armv7l.tar.xz -C /home/pi
+export PATH=${PATH}:/home/pi/node-v10.11.0-linux-armv7l/bin
