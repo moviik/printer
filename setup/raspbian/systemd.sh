d1f491f6159b9769b44a1a9aedb9196747c7b173
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-12-06 14:56:48 +0000

    refactor: rename mik_ to mik- in systemd-services

--- setup/raspbian/systemd.sh
+++ setup/raspbian/systemd.sh
@@ -7,12 +7,12 @@ if [ "$(id -u)" != 0 ]
   exit
 fi
 
-cp ./mik_printer.service /etc/systemd/system
-cp ./mik_dispenser.service /etc/systemd/system
+cp ./mik-printer.service /etc/systemd/system
+cp ./mik-dispenser.service /etc/systemd/system
 
 cp -rp /home/pi/mik-printer /usr/local/bin
 cp -rp /home/pi/mik-dispenser /usr/local/bin
 cp -rp /home/pi/node-v10.11.0-linux-armv7l /usr/local/bin
 
-systemctl enable mik_printer
-systemctl enable mik_dispenser
+systemctl enable mik-printer
+systemctl enable mik-dispenser
