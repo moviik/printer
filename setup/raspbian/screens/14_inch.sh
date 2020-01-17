faa5b1237c1ba5d8bd8e005c6cd76e622ab676ff
Nelson Francisco <nelson_cunha1991@hotmail.com>
2020-01-17 18:11:15 +0000

    deps: Instructions to install 14 inch screen

---
+++ setup/raspbian/screens/14_inch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# screen rotation counterclock
+# must be a shell script because of escaping
+# see https://wiki.lxde.org/en/LXSession
+
+#   display
+xrandr --output HDMI-1 --rotate left
+
+#   touch matrix rotation, needs delay because xorg takes time to detect the touch device
+sleep 10
+xinput set-prop "ILITEK ILITEK-TP" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
