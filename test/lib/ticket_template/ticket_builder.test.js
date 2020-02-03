179cafc72aefeed2dd6cde0d04eb329ff24e2c68
Nelson Francisco <nelson_cunha1991@hotmail.com>
2020-02-03 18:15:28 +0000

    qa: TicketBuilder unset optionsl fields testing

--- test/lib/ticket_template/ticket_builder.test.js
+++ test/lib/ticket_template/ticket_builder.test.js
@@ -37,6 +37,44 @@ describe('Ticket builder', () => {
       ticketBuilder.build({ [option]: optionValue })
     })
 
+    it('should build with optional', (done) => {
+      let setCount = 0
+
+      const option = 'label'
+      const optionValue = 'whatevs'
+
+      const optionalField = 'optional'
+
+      class FakeAdapter extends PrinterAdapter {
+        static openPrinter () {}
+        static closePrinter () {}
+        static printerStatus () { }
+        static getStatusError () { }
+        static getPrinterError () {}
+        static getHandle () {}
+        static freeHandle () {}
+        static setXmlTagValue (handle, tag, value) {
+          setCount++
+          if (setCount === 1) {
+            expect(tag).toEqual(`${optionalField}.hidden`)
+            expect(value).toEqual(1)
+            done()
+          }
+          if (setCount === 2) {
+            expect(tag).toEqual(`${option}.hidden`)
+            expect(value).toEqual(0)
+          }
+          if (setCount === 3) {
+            expect(tag).toEqual(`${option}.text`)
+            expect(value).toEqual(optionValue)
+          }
+        }
+      }
+      const controller = new PrinterController(FakeAdapter, 100, 100)
+      const ticketBuilder = new TicketBuilder(controller, [], [optionalField])
+      ticketBuilder.build({ [option]: optionValue })
+    })
+
     it('should throw if mandatory is not provided', (done) => {
       class FakeAdapter extends PrinterAdapter {
         static openPrinter () {}
